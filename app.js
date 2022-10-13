a1289e4342ed23359d0f377706a31a2564a5bc31
jayhun0521 <jayhun21@gmail.com>
2022-10-12 22:03:53 -0400

    new push

---
+++ app.js
@@ -0,0 +1,29 @@
+var numberOne = 1;
+var numberTwo = 2;
+var numberThree = 3;
+var numberFour = 4;
+var numberFive = 5;
+var numberSix = 6;
+var numberSeven = 7;
+var numberEight = 8;
+var numberNine = 9;
+var numberTen = 10;
+
+
+console.log(numberOne + numberFive);
+console.log (numberTwo + numberSix);
+
+console.log(numberEight - numberFive);
+console.log(numberFour - numberNine);
+
+console.log(numberFive * numberEight);
+console.log(numberSix * numberTen);
+
+console.log(numberSeven / numberFour);
+console.log(numberThree / numberOne);
+
+console.log(++numberEight);
+console.log(++numberTen);
+
+console.log(--numberFive);
+console.log(--numberSeven);
